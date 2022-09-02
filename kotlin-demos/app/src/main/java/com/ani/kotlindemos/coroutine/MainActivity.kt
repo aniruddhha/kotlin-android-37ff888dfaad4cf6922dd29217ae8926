1f73e42dbdf323b64afd7c8c4edcd4ab3c292525
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-09-02 12:13:40 +0530

    coroutines :  advanced

--- kotlin-demos/app/src/main/java/com/ani/kotlindemos/coroutine/MainActivity.kt
+++ kotlin-demos/app/src/main/java/com/ani/kotlindemos/coroutine/MainActivity.kt
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ani.kotlindemos.R
 import kotlinx.coroutines.*
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
+    val handler = CoroutineExceptionHandler { _, throwable -> println(throwable.message)  }
+
+    suspend fun networkCall() {
+        val url = URL("https://www.google.com/")
+        with(url) {
+            readText()
+        }
+    }
 
     fun coroutineDemo() {
+
         GlobalScope.launch {
             // async logic
             delay(1500)
@@ -18,10 +28,51 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun ownScope() {
-        CoroutineScope(Dispatchers.Main).launch {
+        val scp = CoroutineScope(Dispatchers.Main)
+        scp.launch(handler) {
             delay(1800)
             println("In A Dispatcher.IO")
             println("===> ${Thread.currentThread().name}")
+
+            networkCall()
+        }
+    }
+
+    fun cancellable() {
+        val scp = CoroutineScope(Dispatchers.IO)
+        val job: Job = scp.launch(handler) {
+            println("Started Job")
+            delay(1500)
+            println("Completed Job")
+        }
+
+        job.cancel()
+    }
+
+    fun manyTasks() {
+        val scp = CoroutineScope(Dispatchers.IO)
+        scp.launch {
+            val job = launch {
+
+                val task1 = launch {
+                    println("Task1 Started")
+                    delay(2000)
+                    println("Task1 Completed")
+                }
+
+                val task2 = async {
+                    println("Task2 Started")
+                    delay(1200)
+                    println("Task2 Completed")
+
+//                task1.cancel()
+                    return@async true
+                }
+
+                println(task2.await())
+            }
+
+//            job.join()
         }
     }
 
@@ -29,8 +80,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        coroutineDemo()
+        manyTasks()
 
-        ownScope()
     }
 }
