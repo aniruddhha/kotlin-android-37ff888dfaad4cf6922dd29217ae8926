7c84d13f6615329d98758a85ce03e2f9f39826f1
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-09-06 19:50:54 +0530

    runtime permissions

--- jetpack-app/app/src/main/java/com/ani/jetpack/MainActivity.kt
+++ jetpack-app/app/src/main/java/com/ani/jetpack/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.ani.jetpack
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts.*
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.commit
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.ani.jetpack.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel : MainViewModel by viewModels()
+
+    private val requestPermissionLauncher = registerForActivityResult(RequestPermission()) { isGranted ->
+        if(isGranted) {
+            Log.i("@ani", "Permission Granted")
+        }
+    }
 
     private fun viewBindingWay() {
         //private lateinit var binding: ActivityMainBinding
@@ -42,6 +50,35 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                Log.i("@ani", "Permission Granted")
+            }
+
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+
+                Log.i("@ani", "Showing Dialog")
+                val dialog = AlertDialog.Builder(this)
+                    .setPositiveButton("Yes") { di, id -> requestPermissionLauncher.launch(
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    )  }
+                    .setNegativeButton("No") { di, id -> di.dismiss() }
+                    .setMessage("We Need Permission")
+                    .create()
+
+                dialog.show()
+            }
+
+            else -> {
+                requestPermissionLauncher.launch(
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            }
+        }
 
         val binding : ActivityMainBinding  = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
