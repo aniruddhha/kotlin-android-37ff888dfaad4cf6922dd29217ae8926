025e9e81e317d27e6af3b04dfd94d7a8a78488c4
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-09-06 10:50:49 +0530

    jetpack : recycler view

--- jetpack-app/app/src/main/java/com/ani/jetpack/MidFragment.kt
+++ jetpack-app/app/src/main/java/com/ani/jetpack/MidFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import java.time.LocalDate
@@ -22,17 +23,18 @@ class MidFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val rec = view.findViewById<RecyclerView>(R.id.rec)
-        rec.layoutManager = LinearLayoutManager(activity)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            rec.adapter = HiAdapter(
+        val ll = LinearLayoutManager(activity)
+        view.findViewById<RecyclerView>(R.id.rec).apply {
+            layoutManager = ll
+            addItemDecoration(
+                DividerItemDecoration(requireContext(), ll.orientation)
+            )
+            adapter =  HiAdapter(
                 requireContext(),
                 listOf(
                     Msg(from = "abc", msg = "Hey hi", dt = LocalDate.of(2021, 1 ,1).toString()),
                     Msg(from = "par", msg = "I am good", dt = LocalDate.of(2022, 10 ,1).toString()),
                     Msg(from = "xyz", msg = "wow good", dt = LocalDate.of(2022, 11 ,11).toString())
-
                 )
             )
         }
