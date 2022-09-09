c6a9a22a331bcaa88ec85664d1dfa9c45ecbeec2
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-09-09 12:55:51 +0530

    camerax : image capture

--- camera-batch-app/app/src/main/java/com/ani/camera/batch/MainActivity.kt
+++ camera-batch-app/app/src/main/java/com/ani/camera/batch/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.ani.camera.batch
 
+import android.content.ContentValues
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
@@ -14,6 +21,7 @@ import java.util.concurrent.Executors
 class MainActivity : AppCompatActivity() {
 
     private lateinit var cameraExecutor : ExecutorService
+    private var imageCapture: ImageCapture? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +34,11 @@ class MainActivity : AppCompatActivity() {
         cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
+    override fun onDestroy() {
+        cameraExecutor.shutdown()
+        super.onDestroy()
+    }
+
     private fun startPreview() {
 
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
@@ -33,21 +46,56 @@ class MainActivity : AppCompatActivity() {
         cameraProviderFuture.addListener({
             val cameraProvider : ProcessCameraProvider = cameraProviderFuture.get()
 
-            val preview = Preview.Builder()
+            val preview : Preview = Preview.Builder()
                 .build()
                 .also {
                     it.setSurfaceProvider(findViewById<PreviewView>(R.id.previewView).surfaceProvider)
                 }
 
+            imageCapture = ImageCapture.Builder().build()
+
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
             cameraProvider.unbindAll()
-            cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
 
         }, ContextCompat.getMainExecutor(this))
-
     }
 
     private fun capture() {
+        val imageCapture = this.imageCapture ?: return
 
+        val vals = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, "${System.currentTimeMillis()}")
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/AniCam")
+            }
+        }
+
+        val outOps = ImageCapture.OutputFileOptions.Builder(
+            contentResolver,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            vals
+        ).build()
+
+
+        val onSaved : ImageCapture.OnImageSavedCallback = object : ImageCapture.OnImageSavedCallback {
+
+            override fun onImageSaved(res: ImageCapture.OutputFileResults) {
+                Log.i("@ani",  "Success : ${res.savedUri}")
+//                Toast.makeText(this@MainActivity, "Success : ${res.savedUri}", Toast.LENGTH_LONG ).show()
+            }
+
+            override fun onError(exception: ImageCaptureException) {
+//                Toast.makeText(this@MainActivity, "Error In Saving Image", Toast.LENGTH_LONG ).show()
+                Log.i("@ani",  "Error In Saving Image")
+            }
+        }
+
+        imageCapture.takePicture(
+            outOps,
+            cameraExecutor,
+            onSaved
+        )
     }
 }
