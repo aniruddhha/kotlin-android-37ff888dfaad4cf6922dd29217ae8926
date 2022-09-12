3f96ddf802557ccdc818c612df0805874b70572c
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-09-12 14:00:58 +0530

    hilt app : basics and implementation

--- di-hilt/app/src/main/java/com/ani/hiltapp/MainActivity.kt
+++ di-hilt/app/src/main/java/com/ani/hiltapp/MainActivity.kt
@@ -8,15 +8,30 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.commit
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.scopes.ActivityScoped
+import dagger.hilt.android.scopes.ViewModelScoped
+import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var gson : Gson
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,7 +44,7 @@ class MainActivity : AppCompatActivity() {
 @AndroidEntryPoint
 class FriendsFragment : Fragment() {
 
-    private val viewModel by activityViewModels<FriendsViewModel>()
+    private val viewModel : FriendsViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -50,9 +65,22 @@ class FriendsViewModel
 @Inject
 constructor (
     private val local : LocalRepo,
-    private val remote : RemoteRepo
+    private val remote : RemoteRepo,
 ) : ViewModel()
 
+@ViewModelScoped
 class LocalRepo
+@Inject constructor()
 
+@ViewModelScoped
 class RemoteRepo
+@Inject constructor()
+
+@Module
+@InstallIn(ActivityComponent::class)
+class GsonConfigModule {
+
+    @ActivityScoped
+    @Provides
+    fun gson()  = Gson()
+}
